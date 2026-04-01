14717ff2d45a22c27cc2e61c7dd8588d018ba4b1
Nikolay Alferuk <alferukn@gmail.com>
2026-04-01 17:51:59 +0300

    Add fun deallocate

--- src/free-list-alloc.hs
+++ src/free-list-alloc.hs
@@ -8,21 +8,31 @@ data Block = Block {
         bSize :: Size
 } deriving (Show, Eq)
 
-data Allocator = Allocator {
+data AllocatorState = AllocatorState {
         freeBlocks :: [Block]
-}
+} deriving (Show, Eq)
 
-initAllocator :: Size -> Allocator
-initAllocator maxSize = Allocator {
+initAllocator :: Size -> AllocatorState
+initAllocator maxSize = AllocatorState {
         freeBlocks = [Block 0 maxSize]
 }
 
-allocate :: Size -> Allocator -> (Maybe Address, Allocator)
-allocate needSize (Allocator blocks) = 
+allocate :: Size -> AllocatorState -> (Maybe Address, AllocatorState)
+allocate needSize (AllocatorState blocks) =
         let (before, found) = break (\b -> bSize b >= needSize) blocks in
         case found of
-                [] -> (Nothing, Allocator blocks)
-                (targetBlock : after) -> (Just addr, Allocator newFreeBlocks) where
-                        addr = bAddr targetBlock 
+                [] -> (Nothing, AllocatorState blocks)
+                (targetBlock : after) -> (Just addr, AllocatorState newFreeBlocks) where
+                        addr = bAddr targetBlock
                         newFreeBlock = Block (addr + needSize) (bSize targetBlock - needSize)
                         newFreeBlocks = before ++ (if bSize newFreeBlock > 0 then [newFreeBlock] else []) ++ after
+
+deallocate :: Block -> AllocatorState -> AllocatorState
+deallocate (Block deAddr deSize) (AllocatorState freeBlocks) =
+        let endAddr = deAddr + deSize
+            (before, nextBlock : after) = break (\ b -> endAddr <= bAddr b) freeBlocks
+            nextAddr = bAddr nextBlock
+            nextSize = bSize nextBlock
+        in if endAddr == nextAddr
+                then AllocatorState (before ++ [Block deAddr (deSize + nextSize)] ++ after)
+                else AllocatorState (before ++ [Block deAddr deSize] ++ [nextBlock] ++ after)
